f44cfe64e67e996a54f5c41b93be87c6887d6e3e
Muttakee31 <muttakee9@gmail.com>
2018-01-25 05:16:24 +0600

    project done

---
+++ Hangman/pingsr4.h
@@ -0,0 +1,94 @@
+#define US_PORT PORTB
+#define US_PIN PINB
+
+
+
+
+
+
+
+
+
+
+
+
+#define US_DDR  DDRB
+#define US_TRIG PB0
+#define US_ECHO PB1
+
+
+//Calculating width of high pulse in micro second.
+#define US_Time_out  -12
+#define US_NO_OBSTACLE -13
+void US_init();
+void US_trig();
+void US_init()
+{
+	US_DDR|=(1<<US_TRIG);
+}
+
+void US_trig()
+{
+	//Send a 10uS pulse on trigger line
+	
+	US_PORT|=(1<<US_TRIG); //high
+	
+	_delay_us(15);    //wait 15uS
+	
+	US_PORT&=~(1<<US_TRIG); //low
+
+}
+
+uint16_t pulse_width()
+{
+
+	uint32_t i,result;      // Value goes from 0 to 65536.
+
+	//Wait for the rising edge
+	for(i=0;i<600000;i++)
+	{
+		if(!(US_PIN & (1<<US_ECHO)))
+		continue;      //Line is still low, so wait
+		else
+		break;       //High edge detected, so break.
+	}
+
+	if(i==600000)
+	return US_Time_out;     // No Reply found from US sensor, time lapsed
+	
+	//High Edge Found
+
+
+	//Setup Timer1
+	TCCR1A=0X00;
+	TCCR1B=(1<<CS11);      //Prescaler = Fcpu/8
+	TCNT1=0x00;        //Again initialising the counter
+
+	//Counting the width of pulse.
+	
+	//Now wait for the falling edge
+	for(i=0;i<600000;i++)
+	{
+		if(US_PIN & (1<<US_ECHO))
+		{
+			if(TCNT1 > 60000) break; else continue;
+		}
+		else
+		break;
+	}
+
+	if(i==600000)
+	return US_NO_OBSTACLE; //Indicates time out
+
+	//Falling edge found
+
+	result=TCNT1;
+
+	//Stop Timer
+	TCCR1B=0x00;
+
+	if(result > 60000)
+	return US_NO_OBSTACLE; //No obstacle
+	else
+	return (result>>1);
+}
